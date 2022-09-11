e19772e5cd5d0fef1a8a444fb671853e898a2ba4
Paulo César <paulo_cesarcanal@hotmail.com>
2022-09-11 17:30:00 -0300

    MENU MOBILE
    não consegui, não encontrei o erro

--- SETS/mobile-nevbar.js
+++ SETS/mobile-nevbar.js
@@ -5,4 +5,21 @@ class MobileNavbar {
         this.navLinks = document.querySelectorAll(navLinks);
         this.activeClass = "active";
     }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", () => console.log("hey dog"));
+    }
+
+    int() {
+        if(this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
 }
+
+const mobileNavbar = new mobileNavbar(
+    ".mobile-menu",
+    ".nave-list",
+    ".nave-list li"
+);
